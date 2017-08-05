d5c83b634709826c0dc8f28fb1588aa6e3b7d851
Adam <medaman@gmail.com>
2017-08-05 16:18:08 -0700

    successful chat, but unsuccsessful game

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,28 +1,26 @@
 var config = {
-  apiKey: "AIzaSyD-d2LwLO29_1nU4Gc464BFEdlRqt4JLP0",
-  authDomain: "medaman-e56d2.firebaseapp.com",
-  databaseURL: "https://medaman-e56d2.firebaseio.com",
-  projectId: "medaman-e56d2",
-  storageBucket: "medaman-e56d2.appspot.com",
-  messagingSenderId: "510235193408"
+  apiKey: "AIzaSyCU1DcyCq1p8CHutmQ1Ao-f6izMQFoL9hI",
+  authDomain: "rock-paper-scissors-fc667.firebaseapp.com",
+  databaseURL: "https://rock-paper-scissors-fc667.firebaseio.com",
+  projectId: "rock-paper-scissors-fc667",
+  storageBucket: "",
+  messagingSenderId: "102340478271"
 };
 
 firebase.initializeApp(config);
 
 $(document).ready(function() {
 
-  var rps = ["r", "p", "s"]
   var numAvatars = 6;
   var charName;
+  var myAvatar;
 
-  var initialLatestChat = [];
-  var latestChat = initialLatestChat;
-    
+  var latestChat = [];
+  var secondPlayerSat = false;
+  var numGames = 0;
+  var roomNumber = 0;
+
   var database = firebase.database();
-  database.ref().set({
-    latestChat: latestChat
-  });
-
 
   for(var i=1; i<=numAvatars; i++) {
     var newDiv = $("<div>");
@@ -39,48 +37,92 @@ $(document).ready(function() {
     charName = $("#char-name").val().trim();
     if(charName != "") {
       $("#start-screen").css("visibility", "hidden");
-      placeAvatar($(this).attr("value"), "my");
+      myAvatar = $(this).attr("value")
     } else {
       $("#start-comment").html("<h1>Please enter name</h1>")
     }
   });
 
-  function placeAvatar(number, area) {
+  function placeAvatar(avatarNumber, area) {
     var newImg = $("<img>");
-    newImg.attr("src", "assets/images/avatar" + number + ".jpg");
+    newImg.attr("src", "assets/images/avatar" + avatarNumber + ".jpg");
     $("#" + area + "-avatar").html(newImg);
     $("#" + area + "-name").text($("#char-name").val().trim());
   }
 
-  database.ref().on("value", function(snapshot) {
-    latestChat = snapshot.val().latestChat;
-    if (latestChat[0] != charName && latestChat[0]!="firebase-loading-text-value-changer") {
-      var newDiv = $("<div>");
-      newDiv.text(latestChat[0] + ": " + latestChat[1]);
-      $("#chat-area").append(newDiv);
-      $("#chat-area").scrollTop($("#chat-area")[0].scrollHeight);
-    }
-  }, function(errorObject) {
-    console.log(errorObject);
-  });
+  function chatUpdater() {
+    database.ref("room"+roomNumber).on("value", function(snapshot) {
+        latestChat = snapshot.val().latestChat;
+        if (latestChat[0]!="firebase-loading-text-value-changer" && latestChat[0] != "") {
+          var newDiv = $("<div>");
+          newDiv.text(latestChat[0] + ": " + latestChat[1]);
+          $("#chat-area").append(newDiv);
+          $("#chat-area").scrollTop($("#chat-area")[0].scrollHeight);
+        }
+    }, function(errorObject) {
+      console.log(errorObject);
+    });
+  }
+
+/*
+  $(document).on("click", ".sitDown", function() {
+    database.ref("game"+roomNumber).
+    placeAvatar(myAvatar, "opponent");
+  }*/
+
 
   $("#submit-chat").on("click", function() {
     event.preventDefault();
     var chatText = $("#chat-text").val().trim();
     if(chatText != "") {
-      var newDiv = $("<div>");
-      newDiv.text("Me: " + chatText);
-      $("#chat-area").append(newDiv);
-      $("#chat-area").scrollTop($("#chat-area")[0].scrollHeight);
-      $("#chat-text").val("");
-      //this "firebase-loading" thing needs to be done in case the user types the same thing twice or more. If they type the same thing twice or more, the database value was not changing, so the text does not appear more than once. I had to put in this extra code, so everytime anything is added to chat, the value changes for sure. This will break if a user's name is "firebase-loading". That is why i tried to make it as complicated as possible.
-      database.ref().set({
+      database.ref("room"+roomNumber).set({
         latestChat: ["firebase-loading-text-value-changer"]
       });
-      database.ref().set({
+      database.ref("room"+roomNumber).set({
+        myRoomNumber: roomNumber,
         latestChat: [charName, chatText]
       });
     }
+    $("#chat-text").val("");
   })
+
+  $("#rooms").empty();
+  for(var i=1; i<=9; i++) {
+    var newDiv = $("<div>");
+    var leftSeat = createButton("Left");
+    var rightSeat = createButton("Right");
+    newDiv.html("Room number: " + i);
+    newDiv.append(leftSeat);
+    newDiv.append(rightSeat);
+    $("#rooms").append(newDiv);
+  }
+
+  function createButton(side) {
+    var newButton = $("<button>");
+    newButton.addClass("btn btn-default room-buttons");
+    newButton.attr("value", i);
+    newButton.attr("side", side);
+    newButton.text(side + " Side");
+    return newButton;
+  }
+
+  function disconnectPlayer() {
+    database.ref("game"+roomNumber).onDisconnect().set({
+      Player1: ""
+    });
+  }
+
+  $(document).on("click", ".room-buttons", function() {
+    $("#room-selection").css("visibility", "hidden");
+    roomNumber = $(this).attr("value");
+    database.ref("room" + roomNumber).set({ 
+      latestChat: ["",""]
+    })
+    database.ref("room-info").set({
+      numPlayers:
+    })
+    chatUpdater();
+    disconnectPlayer();
+  });
 
 });
